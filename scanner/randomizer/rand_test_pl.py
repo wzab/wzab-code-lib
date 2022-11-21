da7c9dad242ce1eb85de6eaf1120898553cc6b68
Wojciech M. Zabolotny <wzab01@gmail.com>
2022-11-21 18:36:32 +0100

    Added the code with the key to the test sheet.

--- scanner/randomizer/rand_test_pl.py
+++ scanner/randomizer/rand_test_pl.py
@@ -4,12 +4,16 @@ import msgpack
 import os
 header = """
 \\documentclass[11pt]{article}
+\\usepackage{wrapfig}
 \\usepackage[pdftex]{graphicx}
 \\usepackage[utf8]{inputenc}
 \\usepackage[a4paper,left=1cm, right=1cm, top=1cm, bottom=1cm]{geometry}
 \\setlength{\\parindent}{0pt}
 \\begin{document}
 \\noindent
+\\begin{wrapfigure}{r}{3cm}
+\\includegraphics[width=\linewidth]{qkey.png}
+\\end{wrapfigure}
 Please mark the correct answer for the questions below.
 The correct answer should be marked
 by blurring the pattern on the right side of the corresponding letter.
@@ -19,27 +23,25 @@ of the question mark.\\\\
 """
 quests = yaml.load(open("pyt.yaml","r"),Loader=yaml.Loader)
 rqs = quests
+txt = ""
 # Randomize the test
 random.shuffle(rqs)
 for q in rqs:
    random.shuffle(q["answ"])
-# Now write the randomized test to the file
-fo=open("test.tex","w")
-fo.write(header)
 qnum = 1
 qkey = {}
 for q in rqs:
    if qnum != 1:
-      fo.write("\n~\\\\\n")
+      txt+="\n~\\\\\n"
    qans = []
    ac = ord("A")
-   fo.write("\\includegraphics[height=0.7cm]{patterns/"+("%2.2d" % qnum)+".png}\n")
-   fo.write(q["quest"]+" ")
+   txt += "\\includegraphics[height=0.7cm]{patterns/"+("%2.2d" % qnum)+".png}\n"
+   txt += q["quest"]+" "
    for a in q["answ"]:
      if ac != ord("A"):
-        fo.write(", ")
+        txt += ", "
      qans.append((chr(ac), a[0]))
-     fo.write(chr(ac)+")~"+a[2:]+"\n")
+     txt += chr(ac)+")~"+a[2:]+"\n"
      ac += 1
    qkey[qnum] = qans
    qnum = qnum+1
@@ -53,6 +55,9 @@ ct=osy.aes_cbc_pkcs7_encrypt(ht.digest(),zlib.compress(msgpack.packb(qkey)),None
 with open("qkey.bin","wb") as fq:
    fq.write(msgpack.packb(ct))
 os.system("dmtxwrite -s s -f PNG -o qkey.png qkey.bin")
+# Now write the randomized test to the file
+fo=open("test.tex","w")
+fo.write(header+txt)
 fo.write("\\end{document}")
 fo.close()
 print(qkey)
