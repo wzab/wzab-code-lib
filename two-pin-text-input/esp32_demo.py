7310c095ce001bd3325ae6e53b5f75cd2153aaec
Wojciech M. Zabolotny <wzab01@gmail.com>
2024-05-16 19:15:22 +0200

    Code reorganization.

--- two-pin-text-input/esp32_demo.py
+++ two-pin-text-input/esp32_demo.py
@@ -1,11 +1,7 @@
 print("Text inputs with buttons")
 import machine as m
 import collections
-q = collections.deque((),32)
-settling_time = 20 # Set to 1000 to see how does it work
-p1=m.Pin(32,m.Pin.IN, m.Pin.PULL_UP)
-p2=m.Pin(33,m.Pin.IN, m.Pin.PULL_UP)
-t1 = m.Timer(1)
+
 # Define a class implementing states for a state machine
 class eventgen:
     # Constants defining states
@@ -28,88 +24,96 @@ class eventgen:
         self.reps = 0
         self.st = self.IDLE
     def pin_change(self,t): # The function called when the buttons are settled
-        print(self.p1(),self.p2(),self.reps, self.st)
         while True:
+            p1 = self.p1()
+            p2 = self.p2()
+            print(p1,p2,self.reps, self.st)
             if self.st == self.IDLE:
                 print("reps=0")
                 self.reps = 0
-                if self.p1() == 0 and self.p2() == 0:
+                if p1 == 0 and p2 == 0:
                     return
-                elif self.p1() == 1:
+                elif p1 == 1:
                     self.st = self.P1
-                elif self.p2() == 1:
+                elif p2 == 1:
                     self.st = self.P2
             elif self.st == self.P1:
-                if self.p1() == 1 and self.p2() == 0:
+                if p1 == 1 and p2 == 0:
                     return
-                elif self.p1() == 0:
+                elif p1 == 0:
                     self.q.append(("E1",1))
                     self.st = self.IDLE
-                elif self.p2() == 1:
+                elif p2 == 1:
                     self.st = self.P12                
             elif self.st == self.P2:
-                if self.p1() == 0 and self.p2() == 1:
+                if p1 == 0 and p2 == 1:
                     return
-                elif self.p2() == 0:
+                elif p2 == 0:
                     self.q.append(("E2",1))
                     self.st = self.IDLE
-                elif self.p1() == 1:
+                elif p1 == 1:
                     self.st = self.P21
             elif self.st == self.P12:
-                if self.p1() == 1 and self.p2() == 1:
+                if p1 == 1 and p2 == 1:
                     return
-                elif self.p2() == 0:
+                elif p2 == 0:
                     self.reps = self.reps + 1
                     self.st = self.P120
-                elif self.p1() == 0:
+                elif p1 == 0:
                     self.st = self.P121
             elif self.st == self.P121:
-                if self.p1() == 0 and self.p2() == 1:
+                if p1 == 0 and p2 == 1:
                     return
-                if self.p2() == 0:
+                if p2 == 0:
                     self.q.append(("E1f2",int(self.reps)))
                     self.st = self.IDLE
             elif self.st == self.P120:
-                if self.p1() == 1 and self.p2() == 0:
+                if p1 == 1 and p2 == 0:
                     return
-                elif self.p1() == 0:
+                elif p1 == 0:
                     self.q.append(("E12",int(self.reps)))
                     self.st = self.IDLE
-                elif self.p2() == 1:
+                elif p2 == 1:
                     self.st = self.P12
             elif self.st == self.P21:
-                if self.p1() == 1 and self.p2() == 1:
+                if p1 == 1 and p2 == 1:
                     return
-                elif self.p1() == 0:
+                elif p1 == 0:
                     self.reps = self.reps + 1
                     self.st = self.P210
-                elif self.p2() == 0:
+                elif p2 == 0:
                     self.st = self.P212
             elif self.st == self.P212:
-                if self.p1() == 1 and self.p2() == 0:
+                if p1 == 1 and p2 == 0:
                     return
-                if self.p1() == 0:
+                if p1 == 0:
                     self.q.append(("E2f1",int(self.reps)))
                     self.st = self.IDLE
             elif self.st == self.P210:
-                if self.p1() == 0 and self.p2() == 1:
+                if p1 == 0 and p2 == 1:
                     return
-                elif self.p2() == 0:
+                elif p2 == 0:
                     self.q.append(("E21",int(self.reps)))
                     self.st = self.IDLE
-                elif self.p1() == 1:
+                elif p1 == 1:
                     self.st = self.P21
             elif self.st == self.WAIT_IDLE:
-                if self.p1() == 0 and self.p2() == 0:
+                if p1 == 0 and p2 == 0:
                     self.st = self.IDLE
                 else:
                     return
 
-ev = eventgen(p1,p2,q)    
+q = collections.deque((),32)
+settling_time = 20 # Set to 1000 to see how does it work
+Pin1=m.Pin(32,m.Pin.IN, m.Pin.PULL_UP)
+Pin2=m.Pin(33,m.Pin.IN, m.Pin.PULL_UP)
+t1 = m.Timer(1)
+
+ev = eventgen(Pin1,Pin2,q)    
 def p_cb(p):
     t1.init(mode=m.Timer.ONE_SHOT, period=settling_time, callback=ev.pin_change)
-p1.irq(trigger=m.Pin.IRQ_RISING | m.Pin.IRQ_FALLING, handler=p_cb)    
-p2.irq(trigger=m.Pin.IRQ_RISING | m.Pin.IRQ_FALLING, handler=p_cb)
+Pin1.irq(trigger=m.Pin.IRQ_RISING | m.Pin.IRQ_FALLING, handler=p_cb)    
+Pin2.irq(trigger=m.Pin.IRQ_RISING | m.Pin.IRQ_FALLING, handler=p_cb)
 while True:
     try:
         w = q.popleft()
