e0dcdc109e85faef19eb55fc0c2fb460f07a72a2
Wojciech Zabołotny <wzab@WZabHP.nasz.dom>
2026-02-09 11:31:59 +0100

    Added the PSK Watcher sending the notifications to a Jabber account

---
+++ ham-radio/psk-watcher/jcreds.py
@@ -0,0 +1,4 @@
+jid = "sender@jabber.somewhere" # The sending Jabber account
+password = "strong_password" # Password for the sending Jabber account
+target = "recipient@jabber.wherever" # The account on which you want to receive notifications
+
